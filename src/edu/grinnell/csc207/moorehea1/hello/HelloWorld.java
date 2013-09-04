78fec39b8dc8f6f10bae247aa5b4d43febcd62b5
moorehea1 <moorehea1@grinnell.edu>
2013-09-04 09:04:32 -0500

    conflicting changes

--- src/edu/grinnell/csc207/moorehea1/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/moorehea1/hello/HelloWorld.java
@@ -2,7 +2,7 @@ package edu.grinnell.csc207.moorehea1.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Kyle!");
     } // main(String[])
 
 }
