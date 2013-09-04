f7737fc4d1eae3ba27b5b14b96f1147ede715f25
moorehea1 <moorehea1@grinnell.edu>
2013-09-04 08:50:11 -0500

    HelloWorld commit

---
+++ src/edu/grinnell/csc207/moorehea1/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package edu.grinnell.csc207.moorehea1.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+    } // main(String[])
+
+}
